fc30a127e9c9adadde08d612e8a3f24a89b57891
pincoins@gmail.com <9aldufdl>
2017-08-21 02:47:52 +0900

    - local settings - static/media files

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -2,4 +2,36 @@ from .base import *
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static/'),
+]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# Media files (Upload)
+# https://docs.djangoproject.com/en/1.11/topics/files/
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
+
+# Logging
+# https://docs.djangoproject.com/en/1.11/topics/logging/
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
