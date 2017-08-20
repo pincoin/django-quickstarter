47b7ce45ea611892a8ef535e05ce66e4c66b357e
pincoins@gmail.com <9aldufdl>
2017-08-21 04:27:03 +0900

    - i18n support

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -1,5 +1,7 @@
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
 try:
     from .secret import Secret
 except ImportError:
@@ -33,6 +35,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',  # i18n
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -79,6 +82,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+LANGUAGES = [
+    ('en', _('English')),
+]
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/conf'),
+)
 
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
